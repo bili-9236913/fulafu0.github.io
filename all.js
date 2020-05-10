d15c6e22c28fc9f53df903cab46855940f08fc2f
92369113 <3186866987@qq.com>
2020-05-10 20:09:39 +0800

    update all.js.

--- all.js
+++ all.js
@@ -2,7 +2,7 @@
 function(id){
 $('#player').click(function () {
         vUrl = id;  //获取到播放的url
-        var loadstr = '<video width="100%" height="100%"  controls="controls" autobuffer="autobuffer"  autoplay="autoplay" loop="loop">' +
+        var loadstr = '<video width="100%" height="100%"  controls="controls" autobuffer="autobuffer"  preload="metadata" controls muted >' +
                 '<source src='+vUrl+' type="video/mp4"></source></video>';
         layer.open({
             type: 1,
