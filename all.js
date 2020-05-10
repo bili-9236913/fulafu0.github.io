45f7ced4ed12782aab0b57238f37ba8dbfe96c67
92369113 <3186866987@qq.com>
2020-05-10 19:59:05 +0800

    add all.js.

---
+++ all.js
@@ -0,0 +1,16 @@
+//iframe层-多媒体
+function(id){
+$('#player').click(function () {
+        vUrl = id;  //获取到播放的url
+        var loadstr = '<video width="100%" height="100%"  controls="controls" autobuffer="autobuffer"  autoplay="autoplay" loop="loop">' +
+                '<source src='+vUrl+' type="video/mp4"></source></video>';
+        layer.open({
+            type: 1,
+            title: false,
+            area: ['730px', '500px'],
+            shade: [0.8, 'rgb(14, 16, 22)'],
+            skin: 'demo-class',
+            content: loadstr
+        });
+    });
+}
